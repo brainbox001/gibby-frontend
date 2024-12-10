ed24ed945ea75c952f30b9af77310ac161c931b2
Daniel Marcus <momsdboy@gmail.com>
2024-12-10 02:11:05 +0100

    back to default

--- src/Dashboard/DashboardNav.tsx
+++ src/Dashboard/DashboardNav.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function DashboardNavbar({height, col} : {height? : string; col? : string}) {
   return (
-    <div className="relative sm:basis-1/4 sm:h-screen box-border w-21.6">
+    <div className="relative sm:basis-1/4 sm:h-screen">
       <div className={`fixed max-[639px]:bg-white ${col || 'bg-slate-100'} sm:sticky sm:top-0 bottom-0 w-full py-3 ${height || 'sm:h-4/5'}`}>
         <div className="flex justify-around sm:flex-col sm:items-center sm:h-full">
           <div>
